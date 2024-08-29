dacb407dc4a9f2f609365978f10d3e6710d6acb3
pgourishankar07 <pgourishankar07@gmail.com>
2024-08-29 06:54:36 +0530

    generics

---
+++ Oops/Generics.java
@@ -0,0 +1,29 @@
+public class Generics {
+    static class Gen<T1> {
+        T1 val; // you need to declare it as private, or else obj.val = "some other data type"
+                // and use get(),set()
+
+        Gen(T1 v) {
+            val = v;
+        }
+    }
+
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Gen<Integer> g = new Gen<>(5);
+        System.out.println(g.val);
+        Gen<String> g1 = new Gen<>("bubnty");
+        System.out.println(g1.val);
+
+        Integer[] intArray = { 1, 2, 3, 4, 5 };
+        String[] stringArray = { "one", "two", "three", "four", "five" };
+        printArray(intArray); // Output: 1 2 3 4 5
+        printArray(stringArray); // Output: one two three four five
+    }
+}
--- test.java
+++ test.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 class test {
     public static void main(String[] args) {
-        System.out.println('6' - '0');
+        ArrayList a = new ArrayList<>();
+        a.add('a');
+        a.add("abc");
+        a.add(3);
+
+        System.out.println(a);
+        System.out.println((int) ('a'));
     }
 }
